d106d36af0826e3ff0cfd48628d86322be13fd86
Elizeu Ramos <elizeu.nascimento@autbank.com.br>
2022-01-19 12:48:15 -0300

    first commit, estrutura inicial do projeto

---
+++ target/generated-sources/src/main/java/com/vegas/swaggercodegen/model/VersionDetailsDto.java
@@ -0,0 +1,105 @@
+package com.vegas.swaggercodegen.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.vegas.swaggercodegen.model.VersionDto;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * VersionDetailsDto
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-01-19T12:42:32.059475600-03:00[America/Sao_Paulo]")
+
+
+public class VersionDetailsDto   {
+  @JsonProperty("rarity")
+  private Integer rarity = null;
+
+  @JsonProperty("version")
+  private VersionDto version = null;
+
+  public VersionDetailsDto rarity(Integer rarity) {
+    this.rarity = rarity;
+    return this;
+  }
+
+  /**
+   * Get rarity
+   * @return rarity
+   **/
+  @Schema(description = "")
+  
+    public Integer getRarity() {
+    return rarity;
+  }
+
+  public void setRarity(Integer rarity) {
+    this.rarity = rarity;
+  }
+
+  public VersionDetailsDto version(VersionDto version) {
+    this.version = version;
+    return this;
+  }
+
+  /**
+   * Get version
+   * @return version
+   **/
+  @Schema(description = "")
+  
+    @Valid
+    public VersionDto getVersion() {
+    return version;
+  }
+
+  public void setVersion(VersionDto version) {
+    this.version = version;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    VersionDetailsDto versionDetailsDto = (VersionDetailsDto) o;
+    return Objects.equals(this.rarity, versionDetailsDto.rarity) &&
+        Objects.equals(this.version, versionDetailsDto.version);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(rarity, version);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class VersionDetailsDto {\n");
+    
+    sb.append("    rarity: ").append(toIndentedString(rarity)).append("\n");
+    sb.append("    version: ").append(toIndentedString(version)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
---
+++ target/generated-sources/src/main/java/com/vegas/swaggercodegen/model/VersionDto.java
@@ -0,0 +1,103 @@
+package com.vegas.swaggercodegen.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * VersionDto
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-01-19T12:42:32.059475600-03:00[America/Sao_Paulo]")
+
+
+public class VersionDto   {
+  @JsonProperty("name")
+  private String name = null;
+
+  @JsonProperty("url")
+  private String url = null;
+
+  public VersionDto name(String name) {
+    this.name = name;
+    return this;
+  }
+
+  /**
+   * Get name
+   * @return name
+   **/
+  @Schema(description = "")
+  
+    public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public VersionDto url(String url) {
+    this.url = url;
+    return this;
+  }
+
+  /**
+   * Get url
+   * @return url
+   **/
+  @Schema(description = "")
+  
+    public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    VersionDto versionDto = (VersionDto) o;
+    return Objects.equals(this.name, versionDto.name) &&
+        Objects.equals(this.url, versionDto.url);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, url);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class VersionDto {\n");
+    
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    url: ").append(toIndentedString(url)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
